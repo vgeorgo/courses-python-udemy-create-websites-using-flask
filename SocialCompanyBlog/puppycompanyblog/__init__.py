b5c1fb40d98c915a367c8722029ed8d835640739
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-11-19 21:23:18 -0200

    Initial commit SocialCompanyBlog with initial structure, errors handlers and base layouts

---
+++ SocialCompanyBlog/puppycompanyblog/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+from puppycompanyblog.core.views import core
+app.register_blueprint(core)
+
+from puppycompanyblog.error_pages.handlers import error_pages
+app.register_blueprint(error_pages)
