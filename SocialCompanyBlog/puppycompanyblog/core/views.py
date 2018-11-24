714d9a59fc0fc4a26891090e5f5c36336dd3cd92
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-11-24 20:46:51 -0200

    Fixes and layout improvements

--- SocialCompanyBlog/puppycompanyblog/core/views.py
+++ SocialCompanyBlog/puppycompanyblog/core/views.py
@@ -6,7 +6,7 @@ core = Blueprint('core',__name__)
 @core.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=10)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=6)
     return render_template('index.html',blog_posts=blog_posts)
 
 @core.route('/info')
