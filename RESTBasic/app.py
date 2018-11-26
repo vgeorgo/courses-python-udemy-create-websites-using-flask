3f112ccc668b962e2c75937ea135638d99f358b0
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-11-26 13:36:57 -0200

    Improvements on REST Basic

--- RESTBasic/app.py
+++ RESTBasic/app.py
@@ -4,11 +4,34 @@ from flask_restful import Resource,Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello':'world'}
+puppies = []
+class PuppyNames(Resource):
+    def get(self,name):
+        for pup in puppies:
+            if pup['name'] == name:
+                return pup
 
-api.add_resource(HelloWorld,'/')
+        return {'name':None} , 404
+
+    def post(self,name):
+        pup = {'name':name}
+        puppies.append(pup)
+        return pup
+
+    def delete(self,name):
+        for index,pup in enumerate(puppies):
+            if pup['name'] == name:
+                deleted_pup = puppies.pop(index)
+                return {'note':'delete success'}
+
+        return {'note':'delete none'}
+
+class AllNames(Resource):
+    def get(self):
+        return {'puppies':puppies}
+
+api.add_resource(PuppyNames,'/puppy/<string:name>')
+api.add_resource(AllNames,'/puppies')
 
 if __name__ == '__main__':
     app.run(debug=True)
