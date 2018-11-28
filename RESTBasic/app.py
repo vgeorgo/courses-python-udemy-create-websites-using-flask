5460a036e1c310a7f0282b1eda7acf7c25e982d4
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-11-28 20:45:50 -0200

    Rest Authentication, Working with database

--- RESTBasic/app.py
+++ RESTBasic/app.py
@@ -1,34 +1,65 @@
+import os
 from flask import Flask
 from flask_restful import Resource,Api
+from secure_check import authenticate,identity
+from flask_jwt import JWT,jwt_required
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SECRET_KEY'] = 'MY_SECRET_KEY'
+
+#### SQL DATABASE SECTION
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
 api = Api(app)
+jwt = JWT(app,authenticate,identity)
+
+######################################
+
+class Puppy(db.Model):
+    name = db.Column(db.String(80),primary_key=True)
+
+    def __init__(self,name):
+        self.name = name
+
+    def json(self):
+        return {'name':self.name}
+
+######################################
 
 puppies = []
 class PuppyNames(Resource):
     def get(self,name):
-        for pup in puppies:
-            if pup['name'] == name:
-                return pup
+        pup = Puppy.query.filter_by(name=name).first()
+        if pup:
+            return pup.json()
 
         return {'name':None} , 404
 
     def post(self,name):
-        pup = {'name':name}
-        puppies.append(pup)
-        return pup
+        pup = Puppy(name=name)
+        db.session.add(pup)
+        db.session.commit()
+        return pup.json()
 
     def delete(self,name):
-        for index,pup in enumerate(puppies):
-            if pup['name'] == name:
-                deleted_pup = puppies.pop(index)
-                return {'note':'delete success'}
-
-        return {'note':'delete none'}
+        pup = Puppy.query.filter_by(name=name).first()
+        db.session.delete(pup)
+        db.session.commit()
+        return {'note':'delete success'}
 
 class AllNames(Resource):
+    # @jwt_required()
     def get(self):
-        return {'puppies':puppies}
+        puppies = Puppy.query.all()
+        return {'puppies':[pup.json() for pup in puppies]}
 
 api.add_resource(PuppyNames,'/puppy/<string:name>')
 api.add_resource(AllNames,'/puppies')
---
+++ RESTBasic/secure_check.py
@@ -0,0 +1,19 @@
+from user import User
+
+# static data
+# TODO use User model from database
+users = [ User(1,'Jose','mypassword'),
+          User(2,'Mimi','secret')]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and password == user.password:
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
